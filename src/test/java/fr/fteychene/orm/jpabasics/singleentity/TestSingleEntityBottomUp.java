b5228dbe917346c80ad2f16e0b7e9713d0b54ae8
fteychene <francois.teychene@gmail.com>
2017-03-14 13:56:03 +0100

    Add singleentity test with TopDown and BottomUp approach

---
+++ src/test/java/fr/fteychene/orm/jpabasics/singleentity/TestSingleEntityBottomUp.java
@@ -0,0 +1,61 @@
+package fr.fteychene.orm.jpabasics.singleentity;
+
+import fr.fteychene.orm.jpabasics.AbstractJpaTest;
+import fr.fteychene.orm.jpabasics.JpaApplication;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by fteychene on 14/03/17.
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@ContextConfiguration(classes = TestSingleEntityBottomUp.OverrideConfiguration.class)
+@ActiveProfiles("embeddedDatasource")
+public class TestSingleEntityBottomUp extends AbstractJpaTest {
+
+    @Configuration
+    @JpaApplication.OverrideConfiguration
+    @PropertySource("classpath:singleentity/override.properties")
+    static class OverrideConfiguration {
+
+        @Bean
+        @Qualifier("scripts")
+        public String[] startupScripts() {
+            return new String[] {"singleentity/create-db.sql"};
+        }
+    }
+
+    @Test
+    public void _1_create() {
+        User singleTablePerson = new User();
+        singleTablePerson.setFirstName("Augusto");
+        singleTablePerson.setLastName("Rodriguez");
+
+        em().persist(singleTablePerson);
+
+        assertThat(singleTablePerson).isNotNull();
+        assertThat(singleTablePerson.getFirstName()).isEqualTo("Augusto");
+        assertThat(singleTablePerson.getLastName()).isEqualTo("Rodriguez");
+    }
+
+    @Test
+    public void _2_load() {
+        User singleTablePerson = em().find(User.class, 1L);
+
+        assertThat(singleTablePerson).isNotNull();
+        assertThat(singleTablePerson.getFirstName()).isEqualTo("Augusto");
+        assertThat(singleTablePerson.getLastName()).isEqualTo("Rodriguez");
+    }
+}
